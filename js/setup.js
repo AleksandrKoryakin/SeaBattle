fce995da54a3af00831a471fec37bd61bc76c738
Alexander Koryakin <amdb9@mail.ru>
2022-05-30 16:35:03 +0300

    disabling top bottom right left cells added

--- js/setup.js
+++ js/setup.js
@@ -59,72 +59,84 @@ const setup = () => {
   });
 
   // выбор поля для выставления выбранного корабля
-  let cells = [];
-  const cellBtns = document.querySelectorAll('.cell');
-  cellBtns.forEach(element => {
-    cells.push(element);
-  });
-  console.log(cells);
+  const field = [];
+  const row1 = document.querySelector('.row1').querySelectorAll('.cell');
+  const row2 = document.querySelector('.row2').querySelectorAll('.cell');
+  const row3 = document.querySelector('.row3').querySelectorAll('.cell');
+  const row4 = document.querySelector('.row4').querySelectorAll('.cell');
+  const row5 = document.querySelector('.row5').querySelectorAll('.cell');
+  const row6 = document.querySelector('.row6').querySelectorAll('.cell');
+  const row7 = document.querySelector('.row7').querySelectorAll('.cell');
+  const row8 = document.querySelector('.row8').querySelectorAll('.cell');
+  const row9 = document.querySelector('.row9').querySelectorAll('.cell');
+  const row10 = document.querySelector('.row10').querySelectorAll('.cell');
+  field.push(row1);
+  field.push(row2);
+  field.push(row3);
+  field.push(row4);
+  field.push(row5);
+  field.push(row6);
+  field.push(row7);
+  field.push(row8);
+  field.push(row9);
+  field.push(row10);
+  console.log(field);
 
-  let successful = false;
-  let setupSHips = 0;
-  cells.forEach(cell => {
-    cell.addEventListener('click', (event) => {
-      event.preventDefault();
-      successful = false;
-      if (direction) {
-        if (selectedBattleshipLength === 1) {
-          cell.style.backgroundColor = '#FF9900';
-          cell.style.cursor = 'not-allowed';
-          cell.style.pointerEvents = 'none';
-          successful = true;
+  let setupShips = 0;
+  field.forEach(row => {
+    row.forEach(cell => {
+      cell.addEventListener('click', (event) => {
+        event.preventDefault();        
+        // если горизонтально
+        if (direction) {
+          let leftcellcolored = false;
+          // проверка, что корабль влезает в поле
+          if(Array.from(row).indexOf(cell) + selectedBattleshipLength <= 10) {
+            // проверка, что не контактирует с другими кораблями
+            while (selectedBattleshipLength > 0) {
+              cell.style.backgroundColor = '#FF9900';
+              cell.style.cursor = 'not-allowed';
+              cell.style.pointerEvents = 'none';
+              if(Array.from(field).indexOf(row) < 9) { // выделяем поля снизу от корабля
+                field[Array.from(field).indexOf(row) + 1][Array.from(row).indexOf(cell)].style.backgroundColor = 'lightgray';
+                field[Array.from(field).indexOf(row) + 1][Array.from(row).indexOf(cell)].style.cursor = 'not-allowed';
+                field[Array.from(field).indexOf(row) + 1][Array.from(row).indexOf(cell)].style.pointerEvents = 'none';
+              }
+              if(Array.from(field).indexOf(row) > 0) { // выделяем поля сверху от корабля
+                field[Array.from(field).indexOf(row) - 1][Array.from(row).indexOf(cell)].style.backgroundColor = 'lightgray';
+                field[Array.from(field).indexOf(row) - 1][Array.from(row).indexOf(cell)].style.cursor = 'not-allowed';
+                field[Array.from(field).indexOf(row) - 1][Array.from(row).indexOf(cell)].style.pointerEvents = 'none';
+              }
+              if(Array.from(row).indexOf(cell) < 9) { //выделяем поля справа от корабля
+                row[Array.from(row).indexOf(cell) + 1].style.backgroundColor = 'lightgray';
+                row[Array.from(row).indexOf(cell) + 1].style.cursor = 'not-allowed';
+                row[Array.from(row).indexOf(cell) + 1].style.pointerEvents = 'none';
+              }
+              if(Array.from(row).indexOf(cell) === 0) {
+                leftcellcolored = true;
+              }
+              if(Array.from(row).indexOf(cell) > 0 && !leftcellcolored) { //выделяем поля слева от корабля
+                row[Array.from(row).indexOf(cell) - 1].style.backgroundColor = 'lightgray';
+                row[Array.from(row).indexOf(cell) - 1].style.cursor = 'not-allowed';
+                row[Array.from(row).indexOf(cell) - 1].style.pointerEvents = 'none';
+                leftcellcolored = true;
+              }
+              cell = row[Array.from(row).indexOf(cell) + 1];
+              selectedBattleshipLength--;
+            }
+            battleships[selectedBattleshipIndex].classList.add('disabled');
+            battleships[selectedBattleshipIndex].style.outline = 'none';
+            selectedBattleshipIndex = -1;
+            selectedBattleshipLength = 0;
+            setupShips++;
+            leftcellcolored = false;
+          }
         }
-        if (selectedBattleshipLength === 2 && cells.indexOf(cell) % 10 < 9) {
-          cell.style.backgroundColor = '#FF9900';
-          cell.style.cursor = 'not-allowed';
-          cell.style.pointerEvents = 'none';
-          cells[cells.indexOf(cell) + 1].style.backgroundColor = '#FF9900';
-          cells[cells.indexOf(cell) + 1].style.cursor = 'not-allowed';
-          cells[cells.indexOf(cell) + 1].style.pointerEvents = 'none';
-          successful = true;
-        }
-        if (selectedBattleshipLength === 3 && cells.indexOf(cell) % 10 < 8) {
-          cell.style.backgroundColor = '#FF9900';
-          cell.style.cursor = 'not-allowed';
-          cell.style.pointerEvents = 'none';
-          cells[cells.indexOf(cell) + 1].style.backgroundColor = '#FF9900';
-          cells[cells.indexOf(cell) + 1].style.cursor = 'not-allowed';
-          cells[cells.indexOf(cell) + 1].style.pointerEvents = 'none';
-          cells[cells.indexOf(cell) + 2].style.backgroundColor = '#FF9900';
-          cells[cells.indexOf(cell) + 2].style.cursor = 'not-allowed';
-          cells[cells.indexOf(cell) + 2].style.pointerEvents = 'none';
-          successful = true;
-        }
-        if (selectedBattleshipLength === 4 && cells.indexOf(cell) % 10 < 7) {
-          cell.style.backgroundColor = '#FF9900';
-          cell.style.cursor = 'not-allowed';
-          cell.style.pointerEvents = 'none';
-          cells[cells.indexOf(cell) + 1].style.backgroundColor = '#FF9900';
-          cells[cells.indexOf(cell) + 1].style.cursor = 'not-allowed';
-          cells[cells.indexOf(cell) + 1].style.pointerEvents = 'none';
-          cells[cells.indexOf(cell) + 2].style.backgroundColor = '#FF9900';
-          cells[cells.indexOf(cell) + 2].style.cursor = 'not-allowed';
-          cells[cells.indexOf(cell) + 2].style.pointerEvents = 'none';
-          cells[cells.indexOf(cell) + 3].style.backgroundColor = '#FF9900';
-          cells[cells.indexOf(cell) + 3].style.cursor = 'not-allowed';
-          cells[cells.indexOf(cell) + 3].style.pointerEvents = 'none';
-          successful = true;
-        }
-      }
-      if(successful) {
-        battleships[selectedBattleshipIndex].classList.add('disabled');
-        battleships[selectedBattleshipIndex].style.outline = 'none';
-        selectedBattleshipIndex = -1;
-        selectedBattleshipLength = 0;
-        setupSHips++;
-      }
+      })
     })
-  });
+  })
+
+  
 }
 
 setup()
